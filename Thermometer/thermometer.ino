021102201c665160e86e2bf13895dc693e796939
Minh Nguyen <rinhrinh1018@gmail.com>
2020-04-16 16:28:21 -0700

    thermometer refactor code

--- Thermometer/thermometer.ino
+++ Thermometer/thermometer.ino
@@ -13,119 +13,78 @@ const uint8_t red = 16;
 
 void setup()
 {
-  /*lcd.begin(cols, rows)
+    /*lcd.begin(cols, rows)
     lcd: a variable of type LiquidCrystal
     cols: the number of columns that the display has
     rows: the number of rows that the display has 
   */
-  Serial.begin(9600);
 
-  lcd.begin(16, 2);
+    lcd.begin(16, 2);
 
-  pinMode(blue, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(red, OUTPUT);
+    pinMode(blue, OUTPUT);
+    pinMode(green, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(red, OUTPUT);
 }
 void loop()
 {
 
-  int tempReading = analogRead(tempPin);
-  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK); //  Temp Kelvin
-  float tempC = tempK - 273.15;                                                          // Convert Kelvin to Celcius
-  float tempF = (tempC * 9.0) / 5.0 + 32.0;
+    int tempReading = analogRead(tempPin);
+    double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+    tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK); //  Temp Kelvin
+    float tempC = tempK - 273.15;                                                          // Convert Kelvin to Celcius
+    float tempF = (tempC * 9.0) / 5.0 + 32.0;
 
-  // Convert Celcius to Fahrenheit
-  /*  replaced
-    float tempVolts = tempReading * 5.0 / 1024.0;
-    float tempC = (tempVolts - 0.5) * 10.0;
-    float tempF = tempC * 9.0 / 5.0 + 32.0;
-  */
-  // Display Temperature in C
-  lcd.setCursor(0, 0);
-  lcd.print("Temp         C  ");
-  // Display Temperature in F
-  //lcd.print("Temp         F  ");
-  lcd.setCursor(6, 0);
-  // Display Temperature in C
-  lcd.print(tempC);
-  // Display Temperature in F
-  //lcd.print(tempF);
-  delay(500);
-  lcd.setCursor(0, 1);
-  lcd.print("Temp         F  ");
-  lcd.setCursor(6, 1);
-  lcd.print(tempF);
-
-  setRed(tempC);
-  setYellow(tempC);
-  setGreen(tempC);
-  setBlue(tempC);
+    handleDisplayLCD(tempC, tempF);
+    handleDisplayLed(tempC);
 }
 
+void handleDisplayLCD(float tempC, float tempF)
+{
+    // Display Temperature in C
+    lcd.setCursor(0, 0);
+    lcd.print("Temp         C  ");
+    // Display Temperature in F
+    //lcd.print("Temp         F  ");
+    lcd.setCursor(6, 0);
+    // Display Temperature in C
+    lcd.print(tempC);
+    // Display Temperature in F
+    //lcd.print(tempF);
+    delay(500);
+    lcd.setCursor(0, 1);
+    lcd.print("Temp         F  ");
+    lcd.setCursor(6, 1);
+    lcd.print(tempF);
+}
+
+void handleDisplayLed(float tempC)
+{
+    if (tempC > 30)
+    {
+        clearAll();
+        digitalWrite(red, HIGH);
+    }
+    else if ((tempC > 20) && (tempC <= 30))
+    {
+        clearAll();
+        digitalWrite(yellow, HIGH);
+    }
+    else if ((tempC > 10) && (tempC <= 20))
+    {
+        clearAll();
+        digitalWrite(green, HIGH);
+    }
+    else if (tempC <= 10)
+    {
+        clearAll();
+        digitalWrite(blue, HIGH);
+    }
+}
 void clearAll()
 {
-  Serial.println("clearAll");
-  digitalWrite(red, LOW);
-  digitalWrite(yellow, LOW);
-  digitalWrite(green, LOW);
-  digitalWrite(blue, LOW);
-}
-
-void setRed(float tempC)
-{
-  clearAll();
-
-  if (tempC > 30)
-  {
-    digitalWrite(red, HIGH);
-  }
-  else
-  {
     digitalWrite(red, LOW);
-  }
-}
-
-void setYellow(float tempC)
-{
-
-  clearAll();
-
-  if ((tempC > 20) && (tempC <= 30))
-  {
-    digitalWrite(yellow, HIGH);
-  }
-  else
-  {
     digitalWrite(yellow, LOW);
-  }
-}
-
-void setGreen(float tempC)
-{
-  clearAll();
-
-  if ((tempC > 10) && (tempC <= 20))
-  {
-    digitalWrite(green, HIGH);
-  }
-  else
-  {
     digitalWrite(green, LOW);
-  }
-}
-
-void setBlue(float tempC)
-{
-  clearAll();
-
-  if (tempC <= 10)
-  {
-    digitalWrite(blue, HIGH);
-  }
-  else
-  {
     digitalWrite(blue, LOW);
-  }
 }
