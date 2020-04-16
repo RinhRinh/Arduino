110db9e8658a9fde57717ba6c58ad6e3c07d75fd
Minh Nguyen <rinhrinh1018@gmail.com>
2020-04-16 16:31:45 -0700

    thermometer code cleaning

--- Thermometer/thermometer.ino
+++ Thermometer/thermometer.ino
@@ -28,7 +28,6 @@ void setup()
 }
 void loop()
 {
-
     int tempReading = analogRead(tempPin);
     double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
     tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK); //  Temp Kelvin
