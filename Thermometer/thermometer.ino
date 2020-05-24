6f0f0cd8f81e91460264d095b899c3d026ff662e
Minh Nguyen <rinhrinh1018@gmail.com>
2020-05-23 17:23:40 -0700

    Add Clock to LCD and Button to set time

--- Thermometer/thermometer.ino
+++ Thermometer/thermometer.ino
@@ -1,5 +1,4 @@
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(2, 8, 9, 10, 11, 12);
 
 #define BLUE 3
@@ -9,24 +8,45 @@ LiquidCrystal lcd(2, 8, 9, 10, 11, 12);
 const int tempPin = 0;
 int tempType = 0;
 
-const int buttonPin = 16;
-const long intervalButton = 100;
-unsigned long previousMillisButton = 0;
-int buttonNew;
-int buttonOld = 1;
+const int buttonPinThermometer = 15;
+const long intervalButtonThermometer = 100;
+unsigned long previousMillisButtonThermometer = 0;
+int buttonNewThermometer;
+int buttonOldThermometer = 1;
+
+const int buttonPinClockHour = 16;
+const int buttonPinClockMinute = 17;
+const long intervalClockButtonHour = 100;
+const long intervalClockButtonMinute = 100;
+unsigned long previousMillisClockButtonHour = 0;
+unsigned long previousMillisClockButtonMinute = 0;
+int buttonNewClockHour;
+int buttonOldClockHour = 1;
+int buttonNewClockMinute;
+int buttonOldClockMinute = 1;
 
 unsigned long currentMillis = 0;
-unsigned long previousMillisLCD = 0;
-const long intervalLCD = 500;
+
+unsigned long previousMillisThermometer = 0;
+const long intervalThermometer = 650;
 
 int tempReading;
 double tempK;
 float tempC;
 float tempF;
 
+unsigned long previousMillisClock = 0;
+const long intervalClock = 1000;
+int secondDisplay = 0;
+int tenSecondDisplay = 0;
+int minuteDisplay = 0;
+int tenMinuteDisplay = 0;
+int hourDisplay = 2;
+int tenHourDisplay = 1;
+int morningOrEvening = 0;
+
 void setup()
 {
-    Serial.begin(9600);
     lcd.begin(16, 2);
 
     pinMode(RED, OUTPUT);
@@ -38,10 +58,13 @@ void loop()
 {
     // process input
     processTemp();
-    handleButton();
+    handleButtonThermometer();
+    handleClockSetTimeButtons();
+    calculateClockInfo();
 
     // action
-    handleDisplayLCD();
+    handleDisplayLCDForThermometer();
+    handleDisplayLCDForClock();
     handleDisplayLed();
 }
 
@@ -55,17 +78,17 @@ void processTemp()
     tempF = (tempC * 9.0) / 5.0 + 32.0;
 }
 
-void handleButton()
+void handleButtonThermometer()
 {
-    buttonNew = digitalRead(buttonPin);
+    buttonNewThermometer = digitalRead(buttonPinThermometer);
 
     currentMillis = millis();
 
-    if (currentMillis - previousMillisButton >= intervalButton)
+    if (currentMillis - previousMillisButtonThermometer >= intervalButtonThermometer)
     {
-        previousMillisButton = currentMillis;
+        previousMillisButtonThermometer = currentMillis;
 
-        if (buttonOld == 0 && buttonNew == 1)
+        if (buttonOldThermometer == 0 && buttonNewThermometer == 1)
         {
             if (tempType == 0)
             {
@@ -81,18 +104,18 @@ void handleButton()
                 tempType = 0;
             }
         }
+
+        buttonOldThermometer = buttonNewThermometer;
     }
-    buttonOld = buttonNew;
-    delay(100);
 }
 
-void handleDisplayLCD()
+void handleDisplayLCDForThermometer()
 {
     currentMillis = millis();
 
-    if (currentMillis - previousMillisLCD >= intervalLCD)
+    if (currentMillis - previousMillisThermometer >= intervalThermometer)
     {
-        previousMillisLCD = currentMillis;
+        previousMillisThermometer = currentMillis;
 
         if (tempType == 0)
         {
@@ -118,6 +141,121 @@ void handleDisplayLCD()
     }
 }
 
+void calculateClockInfo()
+{
+    if (currentMillis - previousMillisClock >= intervalClock)
+    {
+        secondDisplay = secondDisplay + 1;
+
+        if (secondDisplay > 0 && secondDisplay % 10 == 0)
+        {
+            secondDisplay = 0;
+            tenSecondDisplay = tenSecondDisplay + 1;
+        }
+
+        if (tenSecondDisplay == 6)
+        {
+            secondDisplay = 0;
+            tenSecondDisplay = 0;
+            minuteDisplay = minuteDisplay + 1;
+        }
+
+        if (minuteDisplay > 0 && minuteDisplay % 10 == 0)
+        {
+            minuteDisplay = 0;
+            tenMinuteDisplay = tenMinuteDisplay + 1;
+        }
+
+        if (tenMinuteDisplay == 6)
+        {
+            minuteDisplay = 0;
+            tenMinuteDisplay = 0;
+            hourDisplay = hourDisplay + 1;
+        }
+
+        if (hourDisplay == 10)
+        {
+            hourDisplay = 0;
+            tenHourDisplay = tenHourDisplay + 1;
+        }
+
+        if ((tenHourDisplay == 1) && (hourDisplay == 3))
+        {
+            secondDisplay = 0;
+            tenSecondDisplay = 0;
+            minuteDisplay = 0;
+            tenMinuteDisplay = 0;
+            hourDisplay = 1;
+            tenHourDisplay = 0;
+        }
+
+        if ((tenHourDisplay == 1) && (hourDisplay == 2) && (tenMinuteDisplay == 0) && (minuteDisplay == 0) && (tenSecondDisplay == 0) && (secondDisplay == 0))
+        {
+            morningOrEvening = morningOrEvening + 1;
+        }
+
+        previousMillisClock = currentMillis;
+    }
+}
+
+void handleClockButtons()
+{
+    buttonNewClockHour = digitalRead(buttonPinClockHour);
+    buttonNewClockMinute = digitalRead(buttonPinClockMinute);
+
+    currentMillis = millis();
+
+    if (currentMillis - previousMillisClockButtonHour >= intervalClockButtonHour)
+    {
+        if (buttonOldClockHour == 0 && buttonNewClockHour == 1)
+        {
+            hourDisplay = hourDisplay + 1;
+        }
+
+        if (buttonOldClockMinute == 0 && buttonNewClockMinute == 1)
+        {
+            minuteDisplay = minuteDisplay + 1;
+        }
+        previousMillisClockButtonHour = currentMillis;
+
+        buttonOldClockHour = buttonNewClockHour;
+        buttonOldClockMinute = buttonNewClockMinute;
+    }
+}
+
+void handleDisplayLCDForClock()
+{
+    lcd.setCursor(0, 1);
+    lcd.print("Time");
+    lcd.setCursor(5, 1);
+    lcd.print(tenHourDisplay);
+    lcd.setCursor(6, 1);
+    lcd.print(hourDisplay);
+    lcd.setCursor(7, 1);
+    lcd.print(":");
+    lcd.setCursor(8, 1);
+    lcd.print(tenMinuteDisplay);
+    lcd.setCursor(9, 1);
+    lcd.print(minuteDisplay);
+    lcd.setCursor(10, 1);
+    lcd.print(":");
+    lcd.setCursor(11, 1);
+    lcd.print(tenSecondDisplay);
+    lcd.setCursor(12, 1);
+    lcd.print(secondDisplay);
+
+    if ((morningOrEvening % 2) == 0)
+    {
+        lcd.setCursor(14, 1);
+        lcd.print("AM");
+    }
+    else
+    {
+        lcd.setCursor(14, 1);
+        lcd.print("PM");
+    }
+}
+
 void handleDisplayLed()
 {
     if (tempC > 30)
@@ -126,7 +264,7 @@ void handleDisplayLed()
     }
     else if ((tempC > 20) && (tempC <= 30))
     {
-        setColor(100, 100, 0); // Yellow Color
+        setColor(85, 115, 0); // Yellow Color
     }
     else if ((tempC > 10) && (tempC <= 20))
     {
